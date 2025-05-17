e2171c3dfcf105637b88eb9135f48b33927ecc86
hyeonseokk <47437381+hyeonseokk@users.noreply.github.com>
2025-05-17 07:22:18 +0000

    Deploying to gh-pages from @ hyeonseokk/hyeonseokk.github.io@579f2c66f5b24403b5fe006c60e355d2a5ff277a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,6 +113,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=queGQ5UAAAAJ", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/img/CV.pdf", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
